0ff3cf5decd408c839fd1e4330d86de3f01ea151
soumame <sou.tokumaru@gmail.com>
2023-07-04 12:20:35 +0800

    souto.tk統合に向けて色々変更
    多言語対応
    新しいデザインの追求

---
+++ src/i18n/ui.ts
@@ -0,0 +1,30 @@
+export const languages = {
+    en: 'English',
+    ja: '日本語',
+  };
+
+  export const defaultLang = 'ja';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.works': 'About',
+      'nav.Twitter': 'Twitter',
+      'nav.Outstndrs': 'Team Outstndrs',
+      'nav.Discord': 'Join Community',
+      'nav.soto': 'So Tokumaru',
+      'nav.blog': 'See Blog',
+      'button.close': 'Close',
+    },
+    ja: {
+      'nav.home': 'ホーム',
+      'nav.works': '私について',
+      'nav.Twitter': 'Twitter',
+      'nav.Outstndrs': 'チーム逸般人',
+      'nav.Discord': 'コミュニティに参加',
+      'nav.soto': 'So Tokumaru',
+      'nav.blog': 'ブログを見る',
+      'button.close': '閉じる',
+    },
+
+  } as const;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+			  fancy: ["Lexend Peta"],
+			  serif: ["Noto Serif Japanese"],
+			  mono: ["Noto Sans Mono"],
+			},
+		  },
 	},
-	plugins: [],
+	plugins: [
+		plugin(function({ addVariant }) {
+		  addVariant('optional', '&:optional')
+		  addVariant('hocus', ['&:hover', '&:focus'])
+		  addVariant('inverted-colors', '@media (inverted-colors: inverted)')
+		})
+	  ],
 }
