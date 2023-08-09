759bd79b017211139123082f926efda97f4fc97f
soumame <sou.tokumaru@gmail.com>
2023-08-09 22:31:18 +0800

    Revert "色々なおした"
    This reverts commit ba40bac1c5382ab1472531462246cbad6f12f917.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
-import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
@@ -19,40 +19,28 @@ const getSite = function () {
     if (process.env.CF_PAGES_BRANCH !== 'main') {
       return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
     }
-    return new URL(
-      BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split('.')
-        .slice(1)
-        .join('.')}`
-    ).toString();
+    return new URL(BASE_PATH, `https://${new URL(process.env.CF_PAGES_URL).host.split('.').slice(1).join('.')}`).toString();
   }
   return new URL(BASE_PATH, 'http://localhost:3000').toString();
 };
+
 
 // https://astro.build/config
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
-  integrations: [
-    CoverImageDownloader(),
-    CustomIconDownloader(),
-    FeaturedImageDownloader(),
-    PublicNotionCopier(),
-    tailwind(),
-    image(),
-    react(),
+  integrations: [CoverImageDownloader(), CustomIconDownloader(), FeaturedImageDownloader(), PublicNotionCopier(), tailwind(), image(), react(), 
     sitemap({
-      i18n: {
-        defaultLocale: 'ja', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
-        locales: {
-          ja: 'ja-JP', // The `defaultLocale` value must present in `locales` keys
-          en: 'en-US',
-        },
+    i18n: {
+      defaultLocale: 'ja', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      locales: {
+        ja: 'ja-JP', // The `defaultLocale` value must present in `locales` keys
+        en: 'en-US',
       },
-    }),
-  ],
+    },
+  })],
   experimental: {
-    viewTransitions: true,
-  },
+    viewTransitions: true
+  }
 });
+
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,31 +1,34 @@
 export const languages = {
-  en: 'English',
-  ja: '日本語',
-}
+    en: 'English',
+    ja: '日本語',
+  };
 
-export const defaultLang = 'ja'
+  export const defaultLang = 'ja';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.works': 'About',
+      'nav.Twitter': 'Twitter',
+      'nav.Outstndrs': 'Team Outstndrs',
+      'nav.Discord': 'Join Community',
+      'nav.soto': 'So Tokumaru',
+      'nav.blog': 'See Blog',
+      'button.close': 'Close',
+      'footer.contactMe': 'Are you interested me? please contact below!',
+      
+    },
+    ja: {
+      'nav.home': 'ホーム',
+      'nav.works': '私について',
+      'nav.Twitter': 'Twitter',
+      'nav.Outstndrs': 'チーム逸般人',
+      'nav.Discord': 'コミュニティに参加',
+      'nav.soto': 'So Tokumaru',
+      'nav.blog': 'ブログを見る',
+      'button.close': '閉じる',
+      'footer.contactMe': '興味がありますか？ぜひご連絡ください！',
 
-export const ui = {
-  en: {
-    'nav.home': 'Home',
-    'nav.works': 'About',
-    'nav.Twitter': 'Twitter',
-    'nav.Outstndrs': 'Team Outstndrs',
-    'nav.Discord': 'Join Community',
-    'nav.soto': 'So Tokumaru',
-    'nav.blog': 'See Blog',
-    'button.close': 'Close',
-    'footer.contactMe': 'Are you interested me? please contact below!',
-  },
-  ja: {
-    'nav.home': 'ホーム',
-    'nav.works': '私について',
-    'nav.Twitter': 'Twitter',
-    'nav.Outstndrs': 'チーム逸般人',
-    'nav.Discord': 'コミュニティに参加',
-    'nav.soto': 'So Tokumaru',
-    'nav.blog': 'ブログを見る',
-    'button.close': '閉じる',
-    'footer.contactMe': '興味がありますか？ぜひご連絡ください！',
-  },
-} as const
+    },
+
+  } as const;
